d3a3207f75c869be49a6f7041afa9166f85b2235
Carl <chs1974@gmail.com>
2021-04-24 22:32:06 -0400

    Update vet-track.py

--- vet-track.py
+++ vet-track.py
@@ -16,56 +16,56 @@
 #
 #   Lots of tutorials out there far better than mine, but below are the key steps
 #
-#   (Use raspi-config GUI to Enable the SPI Interface)
+#   	(Use raspi-config GUI to Enable the SPI Interface)
 #	sudo raspi-config 
 #
-#   (Add two key libraries...)
+#   	(Add two key libraries...)
 #	git clone https://github.com/adafruit/Adafruit_CircuitPython_Bundle.git
 #	git clone https://github.com/adafruit/circuitpython.git
 #	
-#   (From <https://www.digitalocean.com/community/tutorials/how-to-set-up-time-synchronization-on-debian-10> )
-#   sudo apt-install ntp
+#   	(From <https://www.digitalocean.com/community/tutorials/how-to-set-up-time-synchronization-on-debian-10> )
+#   	sudo apt-install ntp
 #	sudo systemctl status ntp
 #	 
-#   (Install Python 3)
+#   	(Install Python 3)
 #	sudo apt-get install python3-pip
 #	cd Adafruit_CircuitPython_Bundle/
 #	
-#   (This next step takes ~33 minutes!)
+#   	(This next step takes ~33 minutes!)
 #	sudo ./update-submodules.sh    
 #	cd ..
 #	
-#   (See Adafruit RGB Display Library Documentation)
-#   ( https://buildmedia.readthedocs.org/media/pdf/adafruit-circuitpython-rgb-display/latest/adafruit-circuitpython-rgb-display.pdf)
-#   sudo pip3 install adafruit-circuitpython-rgb-display
+#   	(See Adafruit RGB Display Library Documentation)
+#   	( https://buildmedia.readthedocs.org/media/pdf/adafruit-circuitpython-rgb-display/latest/adafruit-circuitpython-rgb-display.pdf)
+#   	sudo pip3 install adafruit-circuitpython-rgb-display
 #	 
-#   (DejaVu Font family based on the Vera Fonts)
+#   	(DejaVu Font family based on the Vera Fonts)
 #	sudo apt-get install ttf-dejavu
 #	
 #	(PIL is the Python Imaging Library) 
 #	sudo apt-get install python3-pil
 #	
-#   (Python Numpy Library)
+#   	(Python Numpy Library)
 #	sudo apt-get install python3-numpy
 # **********************************************************************************************************
 #	  
 # Copy this document over to your RPI via Putty PSFTP ( https://www.puttygen.com/psftp )
 #	
-#			1.) (Obtain IP address of RPI) 
-#			    ifconfig
-#			2.) (Open PSFTP on laptop)
-#			3.) (Open the connection between your computer and your RPI)
-#			    open 192.168.1.39
-#			4.) (Point to your local directory where this file is saved)
-#			    lcd c:/fromRPI
-#			5.) copy this file to your RPI
-#			    put vet-track.py
-#           6.) (Make the python file executable)
-#               Sudo chmod +x vet-track.py
-#           7.) (Make this python file run at every reboot automatically)	
-#               sudo nano /etc/rc.local
-#               (add the below line of code to your rc.local file...)
-#               sudo python3 /home/pi/stats_time.py &
+#	1.) (Obtain IP address of RPI) 
+#	    ifconfig
+#	2.) (Open PSFTP on laptop)
+#	3.) (Open the connection between your computer and your RPI)
+#	    open 192.168.1.39
+#	4.) (Point to your local directory where this file is saved)
+#	    lcd c:/fromRPI
+#	5.) copy this file to your RPI
+#	    put vet-track.py
+#       6.) (Make the python file executable)
+#           Sudo chmod +x vet-track.py
+#       7.) (Make this python file run at every reboot automatically)	
+#           sudo nano /etc/rc.local
+#           (add the below line of code to your rc.local file...)
+#           sudo python3 /home/pi/stats_time.py &
 # **********************************************************************************************************
 #Code below:
 import time
@@ -127,7 +127,6 @@ backlight.value = True
 
 #primary loop...
 while True:
-
 	# Crypto Tracker
 	scraper = cfscrape.create_scraper()
 	url = 'https://api.cryptonator.com/api/ticker/vet-usd'
@@ -196,7 +195,7 @@ while True:
 
 	#5th Price:
 	#% CHANGE (past 1-hr)
-    #Color Code %change, with "+%" = Green; "-%"=RED
+    	#Color Code %change, with "+%" = Green; "-%"=RED
 	if float_change > 0:
 		draw.text((x, y), Change, font=font, fill="#00FF00") #Lime Green
 	else:
